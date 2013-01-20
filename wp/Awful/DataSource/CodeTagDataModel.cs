819d01a80f00054a1b372a97d155a179a6ee5136
Nathan Simpson <nathanisaac@gmail.com>
2013-01-20 10:38:19 -0600

    [Broken] Updating ReplyBox
    - Adding UserControl to handle replying, tagging, and smilies.

---
+++ wp/Awful/DataSource/CodeTagDataModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Awful.Data
+{
+    public class CodeTagDataModel : CommonDataItem
+    {
+        public CodeTagDataModel() : base() { }
+
+        private string _code;
+        public string Code
+        {
+            get { return _code; }
+            set { SetProperty(ref _code, value, "Code"); }
+        }
+    }
+
+    public class CodeTagCollection : CommonDataGroup
+    {
+        public CodeTagCollection() : base() { }
+    }
+}
